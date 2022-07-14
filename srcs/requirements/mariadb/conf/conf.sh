c4eabaf4135e725b10716d2da28023a1eedde22b
TheodoreNave <theodoreyws@hotmail.fr>
2022-07-14 15:33:38 +0200

    added some conf file and try to connect mariadb + wordpress

--- srcs/requirements/mariadb/conf/conf.sh
+++ srcs/requirements/mariadb/conf/conf.sh
@@ -1,31 +1,27 @@
 #!/bin/sh
 
-# If folder doesn't exist
-if [ ! -d "/run/mysqld"]; then
-
-	mkdir -p /run/mysqld
-	chown -R mysql:mysql /run/mysqld/
+if [ ! -d "/run/mysqld" ]; then
+  mkdir -p /run/mysqld
+  chown -R /run/mysqld
 fi
 
+if [ ! -d /var/lib/mysql/mysql ]; then
 
-# If folder doestn't exist
+  chown -R /var/lib/mysql
+  mysql_install_db --basedir=/usr --datadir=/var/lib/mysql --user=mysql --rpm > /dev/null
 
-if [ ! -d "/var/lib/mysql"]; then
+  tfile=`mktemp`
+  if [ ! -f "setup.sql" ]; then
+      return 1
+  fi
 
-	chown -R mysql:mysql /var/lib/mysql/
-	mysql_install_db  --basedir=/usr --datadir=/var/lib/mysql --user=mysql --rpm > /dev/null
+  /usr/bin/mysqld --user=mysql --bootstrap < /tmp/setup.sql
+  rm -f setup.sql
 fi
 
-
-# Launch with the file setup.sql then delete
-/usr/bin/mysqld --user=mysql --bootstrap < /tmp/setup.sql
-rm -rf setup.sql
-
-
-# Allow connections
+sed -i "s|skip-networking|# skip-networking|g" /etc/my.cnf.d/mariadb-server.cnf
 sed -i "s|.*bind-address\s*=.*|bind-address=0.0.0.0|g" /etc/my.cnf.d/mariadb-server.cnf
-sed -i "s|.*skip-networking.*|skip-networking|g" /etc/my.cnf.d/mariadb-server.cnf
 
-# Execute MySql
+sleep 10
 
 exec /usr/bin/mysqld --user=mysql --console
--- srcs/requirements/wordpress/conf.sh
+++ srcs/requirements/wordpress/conf.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 
+if [ -f ./wordpress/wp-config.php ]
+then
+		echo "ok"
+else
+
+wp core download --allow-root
+
+
+wp config create --dbname="$MYSQL_DATABASE" --dbuser="$MYSQL_USER" --dbpass="$MYSQL_PASSWORD" --dbhost=mysql --dbcharset="utf8" --dbcollate="utf8_general_ci" --allow-root
+wp core install --url="tnave.42.fr" --title="Theodore Nave" --admin_user="${WORDPRESS_ROOT_USER}"\
+	--admin_password="${WORDPRESS_ROOT_PASSWORD}" --admin_email="tnave@student.42.fr" --skip-email --allow-root #Configure le site et l'admin
+wp user create "${WORDPRESS_USER}" --user_pass="${WORDPRESS_PASSWORD}" test@example.com --role=author --allow-root
 
 /usr/sbin/php-fpm7 -F -R
 
-while true;
-    do sleep 10000;
-done
+fi
+
+exec "$@"
