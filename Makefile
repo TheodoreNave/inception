41475b59ec3f7285faa746ce69053d5ccec9e2d0
Theodore nave <theodoreyws@hotmail.fr>
2022-07-20 11:42:00 +0200

    added almost final version without environment variable

--- Makefile
+++ Makefile
@@ -1,3 +1,16 @@
-PROJECT_NAME = "Inception"
+NAME = Inception
 
-.PHONY: start stop
+all:	prune reload
+
+start: echo "127.0.0.1 tnave@42.fr" >> /etc/hosts
+
+stop:
+
+clean:	stop
+
+prune:	clean
+
+reload:
+
+
+.PHONY: start stop clean reload all
--- srcs/requirements/wordpress/conf.sh
+++ srcs/requirements/wordpress/conf.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-# while ! mariadb -hlocalhost -uroot -proot root &>/dev/null; do
-sleep 5
+# while ! mariadb -hlocalhost -u$WORDPRESS_DB_USER -p$WORDPRESS_DB_PASSWORD $MYSQL_DB_NAME &>/dev/null; do
+    sleep 5
 # done
 
 
 # echo "missing wpconfig"
-# wp core download --allow-root
+wp core download --allow-root
 
-wp config create --dbname="root123" --dbuser="root" --dbpass="root" --dbhost=mariadb --dbcharset="utf8" --dbcollate="utf8_general_ci" --allow-root
-wp core install --url="tnave.42.fr" --title="Theodore Nave" --admin_user="tnave"\
-	--admin_password="tnave" --admin_email="tnave@student.42.fr" --skip-email --allow-root #Configure le site et l'admin
-wp user create "tnave" --user_pass="tnave" test@example.com --role=author --allow-root
+wp config create --dbname=$MYSQL_DB_NAME --dbuser=$MYSQL_USER --dbpass=$MYSQL_USER_PASSWORD --dbhost=$MYSQL_ROOT_HOST --dbcharset="utf8" --dbcollate="utf8_general_ci" --allow-root
+wp core install --url=tnave.42.fr --title=tnave --admin_user=$WORDPRESS_ADMIN_USER \
+	--admin_password=$WORDPRESS_ADMIN_PASSWORD --admin_email=tnave@student.42.fr --skip-email --allow-root #Configure le site et l'admin
+wp user create $WORDPRESS_USER --user_pass=$WORDPRESS_PASSWORD test@example.com --role=author --allow-root
 
 
 
