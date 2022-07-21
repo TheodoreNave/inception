17001c84970c31c0121700a4d566728c04ff542e
TheodoreNave <theodoreyws@hotmail.fr>
2022-07-21 16:10:51 +0200

    some cleanup

--- srcs/requirements/wordpress/conf.sh
+++ srcs/requirements/wordpress/conf.sh
@@ -4,26 +4,26 @@
 
 sleep 5
 
-if [ ! -f "/var/www/html/wordpress/wp-config.php" ]; then
+# if [ ! -f "/var/www/html/wordpress/wp-config.php" ]; then
 
 	# Download Wordpress
 	wp core download --allow-root
 
-	# Create wp-config.php
-	wp config create --dbname=$WP_DATABASE_NAME --dbuser=$WP_DATABASE_USR 			\
-					--dbpass=$WP_DATABASE_PWD --dbhost=$MYSQL_HOST 	 			\
-					--dbcharset="utf8" --dbcollate="utf8_general_ci" --allow-root
+	# # Create wp-config.php
+	# wp config create --dbname=$WP_DATABASE_NAME --dbuser=$WP_DATABASE_USR 			\
+	# 				--dbpass=$WP_DATABASE_PWD --dbhost=$MYSQL_HOST 	 			\
+	# 				--dbcharset="utf8" --dbcollate="utf8_general_ci" --allow-root
 
-	# Install Wordpress and create Admin
-	wp core install  --url=tnave.42.fr --title=$WP_TITLE --admin_user=$WP_ADMIN_USR \
-					--admin_password=$WP_ADMIN_PWD --admin_email=$WP_EMAIL 		\
-					--skip-email --allow-root
+	# # Install Wordpress and create Admin
+	# wp core install  --url=tnave.42.fr --title=$WP_TITLE --admin_user=$WP_ADMIN_USR \
+	# 				--admin_password=$WP_ADMIN_PWD --admin_email=$WP_EMAIL 		\
+	# 				--skip-email --allow-root
 
-	# Create Basic User
-	wp user create $WP_USR --user_pass=$WP_PWD test@example.com --role=author --allow-root
+	# # Create Basic User
+	# wp user create $WP_USR --user_pass=$WP_PWD test@example.com --role=author --allow-root
 
-	echo "Finish Wordpress Installation"
-fi
+	# echo "Finish Wordpress Installation"
+# fi
 
 echo "=========All Done========="
 /usr/sbin/php-fpm7 -F -R
